af3fe0733c3ae26203964b06cad7546fa4a64413
Sensanaty <salevic@lu-ka.me>
2020-09-07 13:47:55 +0200

    Rearrange favicon ordering and edit the site meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,13 +6,13 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+            { hid: 'description', name: 'description', content: "Luka Salevic, or Sensanaty, is a freelance fullstack web developer skilled in Rails, Vue, React, and much more." },
             { name: 'msapplication-TileColor', content: 'da532c' },
             { name: 'theme-color', content: '#ffca3a' }
         ],
         link: [
-            { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
             { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+            { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
             { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
             { rel: 'manifest', href: '/site.webmanifest' },
             { rel: 'mask-icon', href: '/safari-pinned-tab.png', color: '#5bbad5' }
