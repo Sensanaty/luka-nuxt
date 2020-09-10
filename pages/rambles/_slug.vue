38b1bd8d5b5764c941193506d799ccee213a8fe2
Sensanaty <salevic@lu-ka.me>
2020-09-10 17:01:24 +0200

    Finish styling the ramble _slug pages

---
+++ pages/rambles/_slug.vue
@@ -0,0 +1,86 @@
+<template>
+        <article>
+            <nuxt-link class="back-link" to="/rambles">Back</nuxt-link>
+            <nuxt-content :document="ramble"/>
+        </article>
+
+</template>
+
+<script>
+export default {
+    async asyncData({ $content, params }) {
+        const ramble = await $content('rambles', params.slug).fetch();
+        return { ramble }
+    },
+    head() {
+        return {
+            title: "LUKA | " + this.ramble.title
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+article {
+    background: $dark-highlight;
+    background: $dark-highlight;
+    width: 65%;
+    margin: 35px auto;
+    padding: 40px;
+}
+
+.back-link {
+    color: $highlight;
+    font-weight: 700;
+    font-size: 2em;
+    text-decoration: none;
+    transition: all 150ms ease-in-out;
+    user-select: none;
+
+    &:hover { color: darken($highlight, 13) }
+}
+
+.nuxt-content {
+    margin-top: 15px;
+
+    h1 {
+        font-size: 3.5em;
+        margin: 0;
+        text-decoration: underline solid $highlight;
+        text-align: center;
+    }
+
+    h2 {
+        font-size: 2em;
+        text-align: center;
+    }
+
+    p {
+        font-size: 1.35em;
+        line-height: 1.3;
+    }
+
+    a {
+        color: $highlight;
+        text-decoration: solid underline $dark-highlight;
+        transition: all 150ms ease-in-out;
+
+        &:hover { text-decoration: solid underline $highlight}
+    }
+
+    // This is the class used by Nuxt Content for the codeblocks
+    .line-numbers {
+        background: $main;
+        border-top-left-radius: 7px;
+        border-bottom-left-radius: 7px;
+        border-left: 5px solid $highlight;
+        text-shadow: none;
+        padding: 35px;
+
+        ::selection {
+            background: $highlight;
+            color: black;
+        }
+    }
+}
+</style>
